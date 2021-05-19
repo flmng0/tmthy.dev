855fa55e0107c2c8ac61ead8031188d144f4f641
Timothy Davis <tmthydvs@gmail.com>
2021-05-19 14:15:29 +0930

    Metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,20 @@
 export default {
-  target: "static",
-  srcDir: "src/",
-  modules: [
-    "@nuxt/content",
-  ],
+  head: {
+    titleTemplate: "%s | Tim's Portfolio",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+  },
+  target: 'static',
+  srcDir: 'src/',
+  modules: ['@nuxt/content'],
   content: {},
   components: true,
-};
+}
