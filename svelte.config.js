dd3ae5ab074faab8468c879081d3edbc3a66d01d
Timothy Davis <tmthydvs@gmail.com>
2021-06-24 08:49:36 +0930

    Fix prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,26 @@ import adapter from '@sveltejs/adapter-static'
 
 import preprocess from 'svelte-preprocess'
 import path from 'path'
+import fs from 'fs'
+
+const sketchesDir = path.join(process.cwd(), 'src/data/sketches')
+
+function getSketchIDs() {
+	const entries = fs.readdirSync(sketchesDir)
+
+	const ids = entries.map((entry) => entry.replace(/\.md$/, ''))
+
+	return ids
+}
+
+function getPages() {
+	let pages = ['*']
+
+	let sketchPages = getSketchIDs().map((id) => `/sketches/${id}`)
+	pages.push(...sketchPages)
+
+	return pages
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +31,12 @@ const config = {
 		adapter: adapter({
 			pages: 'out',
 		}),
+		prerender: {
+			enabled: true,
+			crawl: true,
+			force: true,
+			pages: getPages(),
+		},
 		target: 'body',
 		vite: {
 			resolve: {
