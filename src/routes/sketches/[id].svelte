42359e5938ff15be639c3cad5c2ef6d765e382ca
Timothy Davis <tmthydvs@gmail.com>
2021-06-23 18:41:04 +0930

    Get canvas to work on sketch pages

--- src/routes/sketches/[id].svelte
+++ src/routes/sketches/[id].svelte
@@ -21,10 +21,33 @@
 </script>
 
 <script>
+	import { page } from '$app/stores'
+	import { onMount } from 'svelte'
+
 	export let name
 	export let brief
 	export let contentHtml
+
+	const id = $page.params.id
+
+	onMount(() => {
+		const scriptHandle = document.createElement('script')
+		scriptHandle.type = 'module'
+
+		// Uses a ?reloadfix=<TIMESPEC> query to force the browser to fully
+		// reload the script, including the source.
+		const now = new Date().getTime()
+		scriptHandle.src = `/sketches/${id}.js?reloadfix=${now}`
+
+		document.body.appendChild(scriptHandle)
+
+		return () => {
+			document.body.removeChild(scriptHandle)
+		}
+	})
 </script>
+
+<canvas id="sketch-canvas" width="800" height="800" />
 
 <main>
 	<h1>{name}</h1>
@@ -32,3 +55,13 @@
 
 	{@html contentHtml}
 </main>
+
+<style lang="scss">
+	#sketch-canvas {
+		width: min(100%, 800px);
+
+		background-color: white;
+
+		box-shadow: 0 2px 8px var(--color-shadow);
+	}
+</style>
