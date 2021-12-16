731ed66600afb5fc8ae9c1c713eeff7466cf554c
Timothy Davis <44032837+flmng0@users.noreply.github.com>
2021-12-16 22:39:13 +0930

    Notes for sketches

---
+++ src/routes/sketches/[id].svelte
@@ -0,0 +1,4 @@
+<!-- Contents of a sketch. Include a description.
+    See https://github.com/flmng0/flmng0.github.io/blob/1e8d97ffa4be867e110949aba4d3879a4bd70fea/src/routes/sketches/%5Bid%5D.svelte
+    and src/routes/api/sketches(/[id])?.json.ts.
+-->
---
+++ src/routes/sketches/index.svelte
@@ -0,0 +1,10 @@
+<!-- Load a list of sketches into a CardList,
+    where each sketch is a DisplayCard.
+
+    Also, make sure to put some description text
+    at the top of the page, to explain what a
+    "sketch" represents, especially versus a project.
+
+    The above paragraph also makes me consider putting
+    sketches and projects onto the same page.
+-->
