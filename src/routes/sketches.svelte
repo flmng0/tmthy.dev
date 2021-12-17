bcea371539df6f56f43f3223f7b7b7f9a0a4aaa5
Timothy Davis <tmthydvs@gmail.com>
2021-12-17 10:49:57 +0930

    Implement sketches index route. TODO sketch page

---
+++ src/routes/sketches.svelte
@@ -0,0 +1,58 @@
+<!-- Load a list of sketches into a CardList,
+    where each sketch is a DisplayCard.
+
+    Also, make sure to put some description text
+    at the top of the page, to explain what a
+    "sketch" represents, especially versus a project.
+
+    The above paragraph also makes me consider putting
+    sketches and projects onto the same page.
+-->
+<script lang="ts" context="module">
+	export type Sketch = {
+		id: string
+		name: string
+		brief?: string
+		imagePath?: string
+	}
+
+	export async function load({ fetch }) {
+		const request = await fetch('/api/sketches.json')
+
+		if (request.ok) {
+			const data = await request.json()
+			return {
+				props: {
+					...data,
+				},
+			}
+		}
+
+		return {
+			status: request.status,
+			error: new Error('failed to load sketch directory'),
+		}
+	}
+</script>
+
+<script lang="ts">
+	import CardList from '$components/CardList.svelte'
+	import DisplayCard from '$components/DisplayCard.svelte'
+
+	export let sketches: Array<Sketch>
+
+	const cardProps = (sketch: Sketch): any => ({
+		name: sketch.name,
+		href: `/sketches/${sketch.id}`,
+		description: sketch.brief,
+		imageSrc: sketch.imagePath,
+	})
+</script>
+
+<main>
+	<CardList>
+		{#each sketches as sketch}
+			<DisplayCard {...cardProps(sketch)} />
+		{/each}
+	</CardList>
+</main>
