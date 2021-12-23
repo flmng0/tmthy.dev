a99d864e5ac33ec98b318607c1d428e41a99f186
Timothy Davis <tmthydvs@gmail.com>
2021-12-23 17:25:48 +0930

    Restore WIP message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
 	import Hero from "$components/index/Hero.svelte";
-	import Main from "$components/Main.svelte";
 	import { siteName } from "$lib/consts";
+	import { onMount } from "svelte";
+	import { quintOut } from "svelte/easing";
+	import { fly } from "svelte/transition";
 
-	let heroComplete = false;
+	let start = null;
+	onMount(() => {
+		start = {};
+	});
 </script>
 
 <svelte:head>
 	<title>{siteName}</title>
 </svelte:head>
 
-<Hero on:complete={() => (heroComplete = true)} />
+<Hero />
 
-<Main intro={false} />
+{#if start}
+	<p in:fly={{ delay: 3000, duration: 1000, easing: quintOut, x: -200 }} class="wip-text">
+		This site is currently in development, but you can follow its progress on
+		<a href="https://github.com/flmng0/flmng0.github.io">GitHub</a>.
+	</p>
+{/if}
+
+<style lang="scss">
+	.wip-text {
+		position: fixed;
+		bottom: 5em;
+		font-size: 1.2em;
+		font-weight: 200;
+
+		left: 50%;
+		transform: translateX(-50%);
+
+		width: min(100%, 30ch);
+		color: var(--color-fg-secondary);
+
+		a {
+			color: var(--color-main);
+		}
+	}
+</style>
