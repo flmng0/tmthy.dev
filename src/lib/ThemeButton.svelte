ee8962faa33913cbeb007fd12665d030efa6fe91
Timothy Davis <tmthydvs@gmail.com>
2022-08-24 14:37:34 +0930

    Add theme button and store

---
+++ src/lib/ThemeButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { theme } from '$lib/stores'
+	import FeatherIcon from './FeatherIcon.svelte'
+
+	const toggleTheme = () => {
+		theme.update((t) => (t === 'dark' ? 'light' : 'dark'))
+	}
+</script>
+
+<button class="theme" on:click={toggleTheme}>
+	<FeatherIcon key={$theme === 'dark' ? 'sun' : 'moon'} width="1.3em" height="1.3em" />
+</button>
+
+<style>
+	.theme {
+		all: unset;
+	}
+</style>
