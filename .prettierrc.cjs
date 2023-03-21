c4382e05beb0e94175d40c2009c948b6fea4989f
Timothy Davis <tmthydvs@gmail.com>
2023-03-21 15:59:55 +0930

    Remove svelte plugin from prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,13 +6,9 @@ module.exports = {
     printWidth: 100,
     htmlWhitespaceSensitivity: 'css',
     plugins: [
-        require.resolve('prettier-plugin-svelte'),
         require.resolve('prettier-plugin-tailwindcss'),
         require.resolve('prettier-plugin-astro'),
     ],
     pluginSearchDirs: ['.'],
-    overrides: [
-        { files: '*.svelte', options: { parser: 'svelte' } },
-        { files: '*.astro', options: { parser: 'astro' } },
-    ],
+    overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
 }
