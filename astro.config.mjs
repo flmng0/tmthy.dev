abec3af28d16851c217cdaa420f789e958be4a5c
Timothy Davis <tmthydvs@gmail.com>
2023-03-22 16:36:41 +0930

    Configure cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
 
+import cloudflare from '@astrojs/cloudflare'
+
 import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 
@@ -7,6 +9,9 @@ import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 
 export default defineConfig({
+    output: 'server',
+    adapter: cloudflare(),
+
     integrations: [tailwind(), solidJs()],
     markdown: {
         remarkPlugins: [remarkMath],
