63b221a0fe480abd3f791aa5925fd797c8aa14c2
Timothy Davis <tmthydvs@gmail.com>
2023-03-31 00:00:58 +0930

    Use "custom" colors only

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+        colors: {
+            white: colors.white,
+            black: "#252224",
+            gray: colors.gray,
+            pink: "#FFB3DF",
+            green: "#53C655"
+        },
         extend: {},
     },
     plugins: [require('@tailwindcss/typography')],
