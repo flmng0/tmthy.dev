a1422fb3c938a2640c6e3fc8b695037a204c2d15
Timothy Davis <tmthydvs@gmail.com>
2023-06-15 16:44:58 +0930

    Convert to TypeScript, load sketches

---
+++ src/routes/(site)/sketches/+layout.server.ts
@@ -0,0 +1,23 @@
+import fs from 'fs/promises'
+import { baseDir, getData } from '$lib/data'
+import type { LayoutServerLoad } from './$types';
+
+export const prerender = true;
+
+export const load = (async () => {
+    const entries = await fs.readdir(baseDir('sketches'))
+
+    const mdFiles = entries.filter((entry) => entry.endsWith('.md'))
+
+    const sketchFutures = mdFiles.map(async (entry) => {
+        const slug = entry.slice(0, -'.md'.length)
+
+        const data = await getData('sketches', slug)
+
+        return data
+    })
+
+    return { 
+        sketches: await Promise.all(sketchFutures)
+    }
+}) satisfies LayoutServerLoad
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import * as md from 'vite-plugin-markdown'
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [md.plugin({ mode: md.Mode.HTML  }), sveltekit()],
 })
