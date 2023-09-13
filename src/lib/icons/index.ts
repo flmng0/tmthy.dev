2adb901586b1d8e59a22b8bbae15b8dfc8792d8c
Timothy Davis <tmthydvs@gmail.com>
2023-09-13 16:47:28 +0930

    Restart from scratch with Bun

--- src/lib/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MdiClose } from './MdiClose.svelte'
-export { default as MdiEmail } from './MdiEmail.svelte'
-export { default as MdiGitHub } from './MdiGitHub.svelte'
-export { default as MdiLinkedIn } from './MdiLinkedIn.svelte'
--- src/routes/(site)/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Navigation from './Navigation.svelte'
-</script>
-
-<header class="sticky top-0 z-50 mb-8">
-    <Navigation /><!-- client:media="(max-width: 768px)" /> -->
-</header>
-<slot />
