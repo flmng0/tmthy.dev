894d66daea43400400d752bacf577788b921b64f
Timothy Davis <tmthydvs@gmail.com>
2023-11-03 11:32:58 +0930

    Swap to using unplugin-icons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 
+import Icons from "unplugin-icons/vite";
+
+import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()]
+  vite: {
+    plugins: [
+      Icons({
+        scale: 1.2,
+        defaultClass: "icon",
+        compiler: "astro",
+      }),
+    ],
+  },
+  integrations: [svelte(), mdx()],
 });
