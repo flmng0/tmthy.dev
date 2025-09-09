cbaa6636959c6736de2367feb50b6e93b6d7776f
Timothy Davis <tmthydvs@gmail.com>
2025-09-10 00:14:31 +0930

    Update dependencies and formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
-    import appState from '$lib/appState.svelte'
-    import ContentWrapper from '$lib/ContentWrapper.svelte'
+  import appState from "$lib/appState.svelte";
+  import ContentWrapper from "$lib/ContentWrapper.svelte";
 
-    appState.title = 'tmthy.dev'
+  appState.title = "tmthy.dev";
 </script>
 
 <svelte:head>
-    <title>Tim Davis' Web Development Portfolio | tmthy.dev</title>
-    <meta
-        name="description"
-        content="Learn more about a passionate, broadly skilled developer from Australia. Tim Davis' porfolio website."
-    />
+  <title>Tim Davis' Web Development Portfolio | tmthy.dev</title>
+  <meta
+    name="description"
+    content="Learn more about a passionate, broadly skilled developer from Australia. Tim Davis' porfolio website."
+  />
 </svelte:head>
 
 <ContentWrapper>
@@ -23,18 +23,18 @@
 </ContentWrapper>
 
 <style>
-    main {
-        display: grid;
-        width: min(66ch, 100%);
-        height: 100%;
-        align-items: end;
-        margin-inline: auto;
-    }
+  main {
+    display: grid;
+    width: min(66ch, 100%);
+    height: 100%;
+    align-items: end;
+    margin-inline: auto;
+  }
 
-    hgroup {
-        margin-inline: 1em;
-        border-radius: 0.3em;
-        border-width: thin;
-        padding: 0.8em 1.2em;
-    }
+  hgroup {
+    margin-inline: 1em;
+    border-radius: 0.3em;
+    border-width: thin;
+    padding: 0.8em 1.2em;
+  }
 </style>
