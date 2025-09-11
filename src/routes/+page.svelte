df9b448cebd048eeb35e0c548b6ecf261d7d8626
Timothy Davis <tmthydvs@gmail.com>
2025-09-11 13:28:48 +0930

    Dynamic font, cleaner home page, and test focus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,28 +12,3 @@
     content="Learn more about a passionate, broadly skilled developer from Australia. Tim Davis' porfolio website."
   />
 </svelte:head>
-
-<ContentWrapper>
-  <main>
-    <hgroup class="glass">
-      <h1>Hello, I'm Tim Davis!</h1>
-    </hgroup>
-  </main>
-</ContentWrapper>
-
-<style>
-  main {
-    display: grid;
-    width: min(66ch, 100%);
-    height: 100%;
-    align-items: end;
-    margin-inline: auto;
-  }
-
-  hgroup {
-    margin-inline: 1em;
-    border-radius: 0.3em;
-    border-width: thin;
-    padding: 0.8em 1.2em;
-  }
-</style>
