2587817c3321f9a7ff90729a54b48662e43e0d90
Timothy Davis <tmthydvs@gmail.com>
2025-09-23 23:56:54 +0930

    Front-facing sketch API mock-up

--- pages/test.cljs
+++ pages/test.cljs
@@ -1,10 +1,10 @@
 (ns test
   (:require [sketch :refer [run-sketch]]))
 
-(run-sketch 
-  nil
-  (fn [state model]
-    (println model)
-    ))
-    
-    
+
+(run-sketch
+  {:update (fn [{:keys [model]}] (inc model))
+   :seed 0
+   :draw (fn [{:keys [model] :as state}] 
+           (when (zero? (rem model 50)) (println model)))})
+
