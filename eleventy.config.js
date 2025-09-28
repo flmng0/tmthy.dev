98e63b5eba019a7d6e1858cc2e06b7f273a9853c
Timothy Davis <tmthydvs@gmail.com>
2025-09-28 22:23:12 +0930

    Add social links

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,22 @@
-import EleventyPluginVite from "@11ty/eleventy-plugin-vite";
+import pluginVite from "@11ty/eleventy-plugin-vite";
+import pluginIcons from "eleventy-plugin-icons";
 import tailwindcss from "@tailwindcss/vite";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyPluginVite, {
+  eleventyConfig.addPlugin(pluginVite, {
     viteOptions: {
       plugins: [tailwindcss()],
     },
+  });
+
+  eleventyConfig.addPlugin(pluginIcons, {
+    sources: [
+      {
+        name: "lucide",
+        path: "node_modules/lucide-static/icons",
+        default: true,
+      },
+    ],
   });
 
   eleventyConfig.addBundle("html");
